4e13de71c3616530dc75ddb889120959718e4678
Daniel Worsnup <worsnupd@gmail.com>
2016-02-12 14:30:25 -0500

    Add vendor libraries and add clean task to gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,26 @@ var filter = require('gulp-filter');
 var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var htmlmin = require('gulp-htmlmin');
+
+var vendorJs = [
+    'node_modules/angular/angular.min.js',
+    'node_modules/angular-route/angular-route.min.js'
+];
+
+var vendorCss = [
+    'node_modules/bootstrap/dist/css/bootstrap.min.css'
+];
+
+function handleError(error) {
+    console.log(error.toString());
+    this.emit('end');
+}
+
+// Clean the build/ directory
+gulp.task('clean', function() {
+    return del([
+        'build/*'
+    ]);
+});
+
+gulp.task('default', []);
