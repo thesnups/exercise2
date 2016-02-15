edba046d477c86243c62e01fb88b3f5e11192412
Daniel Worsnup <worsnupd@gmail.com>
2016-02-15 09:46:00 -0500

    Enabled script mangling

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('scripts', function() {
         .pipe(plugins.angularFilesort())
         .pipe(plugins.addSrc.prepend(vendorJs)) // Add vendor scripts to beginning of stream
         .pipe(plugins.concat(jsOutFile))
-        .pipe(plugins.uglify({ preserveComments: 'license', mangle: false })).on('error', handleError)
+        .pipe(plugins.uglify({ preserveComments: 'license' })).on('error', handleError)
         .pipe(gulp.dest('build'));
 });
 
