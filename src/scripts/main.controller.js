4611dbe3b49381ed2325b0e4b7c0034e32c29bbf
Daniel Worsnup <worsnupd@gmail.com>
2016-02-14 21:14:39 -0500

    Added retrieval of definitions of startup words.

--- src/scripts/main.controller.js
+++ src/scripts/main.controller.js
@@ -5,12 +5,27 @@
         .module('app')
         .controller('MainController', MainController);
 
-    function MainController($scope, dict) {
-        dict.define('rent')
-            .then(function(data) {
-                $scope.test = data;
-            }, function(data) {
-                alert(data);
-            });
+    function MainController(dict) {
+        var vm = this;
+        vm.words = {};
+        vm.define = define;
+
+        activate();
+
+        function activate() {
+            var startupWords = ['rent', 'apartment', 'dominion', 'enterprise', 'angular', 'javascript'];
+            for(var i = 0; i < startupWords.length; ++i) {
+                define(startupWords[i])
+            }
+        }
+
+        function define(word) {
+            dict.define(word)
+                .then(function(data) {
+                    vm.words[word] = data;
+                }, function(data) {
+                    alert(data);
+                });
+        }
     }
 })();
