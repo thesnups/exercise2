7b1ef769f6dc9ec68554a34595ce458bc85bec13
Daniel Worsnup <worsnupd@gmail.com>
2016-02-17 23:48:27 -0500

    Remove custom isArray in favor of angular.isArray

--- src/scripts/dict.provider.js
+++ src/scripts/dict.provider.js
@@ -12,10 +12,6 @@
             var baseUrl = 'http://api.pearson.com/v2/dictionaries/entries';
             var allowedDatasets = ['ldoce5', 'lasde', 'wordwise', 'laad3']; // These dictionaries provide English definitions of words
 
-            var isArray = function(obj) {
-                return Object.prototype.toString.call(obj) === '[object Array]';
-            }
-
             // Function: hitApi
             // Description: Requests the definitions of <word> from the API
             // Returns: A promise object that will resolve when the response is received
@@ -51,7 +47,7 @@
                         }
 
 
-                        if(datasetOk && result.hasOwnProperty("senses") && isArray(result.senses) && 0 !== result.senses.length) {
+                        if(datasetOk && result.hasOwnProperty("senses") && angular.isArray(result.senses) && 0 !== result.senses.length) {
                             for(var j = 0; j < result.senses.length; ++j) {
                                 var sense = result.senses[j];
 
@@ -59,7 +55,7 @@
                                     var def = sense.definition;
 
                                     // definition property returned from API could be an array of strings or a single string
-                                    if(isArray(def)) {
+                                    if(angular.isArray(def)) {
                                         for(var k = 0; k < def.length; ++k) {
                                             definitions.push(def[k]);
                                         }
